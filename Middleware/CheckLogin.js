ee3d4f66c361a588853f8fb6f18da4c87bf9474c
kaikai6901 <huyhoangbk64@gmail.com>
2023-03-14 22:20:49 +0700

    fix token

--- Middleware/CheckLogin.js
+++ Middleware/CheckLogin.js
@@ -40,7 +40,7 @@ var CheckLoginForLogout=(req,res,next)=>{
            
             let token=req.headers.token
             //token=token.slice(1,token.length-1)
-            let data=jwt.verify(token,'nghiango')
+            let data=jwt.verify(token,'huyhoang')
             Account.findById(data._id)
             .then(data=>{
             if(!data){
@@ -72,7 +72,7 @@ var CheckLoginNoData=(req,res,next)=>{
          
             let token=req.headers.token
            // token=token.slice(1,token.length-1)
-            let data=jwt.verify(token,'nghiango')
+            let data=jwt.verify(token,'huyhoang')
             console.log(data)
             Account.findById(data._id)
             .then(data=>{
